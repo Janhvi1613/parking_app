2b0886c82d883f811f91e192ea9717d544726501
Janhvi1613 <janhvisingh16sep@gmail.com>
2025-06-26 17:43:41 +0530

    created main.py

--- main.py
+++ main.py
@@ -5,7 +5,10 @@ app=Flask(__name__)
 
 app.app_context().push()
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/', methods=['GET'])
 def home():
     if request.method == 'GET':
         return 'This is Home Page'
+    
+if __name__ == '__main__':
+    app.run(host='0.0.0.0',port=8000,debug=True)   
